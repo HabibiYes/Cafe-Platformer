64e35cdb9d2036a1ac83200fe274122342041e66
HabibiYes <calebfparr@gmail.com>
2025-09-04 10:16:00 -0500

    Jumping animation

--- Cafe Platformer/Assets/Player/Scripts/Jumping.cs
+++ Cafe Platformer/Assets/Player/Scripts/Jumping.cs
@@ -29,17 +29,29 @@ public class Jumping : MonoBehaviour
             {
                 isJumping = false;
                 player.rb.linearVelocity = new Vector3(player.rb.linearVelocity.x, 0, player.rb.linearVelocity.z);
+
+                // Trigger landing animation
+                player.animator.SetTrigger("Land");
+                player.animator.ResetTrigger("Jump");
             }
             else if (player.playerMovement.IsGrounded(false))
             {
                 isJumping = false;
+
+                // Trigger landing animation
+                player.animator.SetTrigger("Land");
+                player.animator.ResetTrigger("Jump");
             }
         }
     }
 
     public void Jump()
     {
-        player.rb.linearVelocity = new Vector3(player.rb.linearVelocity.x, player.rb.linearVelocity.y + jumpForce * player.rb.mass, player.rb.linearVelocity.z);
         isJumping = true;
+        player.rb.linearVelocity = new Vector3(player.rb.linearVelocity.x, player.rb.linearVelocity.y + jumpForce * player.rb.mass, player.rb.linearVelocity.z);
+
+        // Trigger jump animation
+        player.animator.SetTrigger("Jump");
+        player.animator.ResetTrigger("Land");
     }
 }
