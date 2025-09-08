ac8f39e31e4a38eb65c24a5f7a3c489ef7f635b8
HabibiYes <calebfparr@gmail.com>
2025-09-08 10:40:02 -0500

    Customers order drinks

--- Cafe Platformer/Assets/Customer/Scripts/Customer.cs
+++ Cafe Platformer/Assets/Customer/Scripts/Customer.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.AI;
 
 [RequireComponent(typeof(ChooseSpot))]
+[RequireComponent(typeof(OrderDrink))]
 public class Customer : MonoBehaviour
 {
     [HideInInspector] public Vector3 spot;
@@ -9,12 +10,15 @@ public class Customer : MonoBehaviour
     [HideInInspector] public NavMeshAgent agent;
 
     private ChooseSpot chooseSpot;
+    private OrderDrink orderDrink;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
 
+        // Get components
         chooseSpot = GetComponent<ChooseSpot>();
+        orderDrink = GetComponent<OrderDrink>();
     }
 
     private void Start()
@@ -22,5 +26,15 @@ public class Customer : MonoBehaviour
         // Find spot and set destination to it
         chooseSpot.FindClosestSpot();
         agent.SetDestination(spot);
+    }
+
+    private void Update()
+    {
+        // Check distance to spot and order if close
+        if (agent.remainingDistance < 0.1f && orderDrink.orderedDrink == null)
+        {
+            orderDrink.Order();
+            Debug.Log(orderDrink.orderedDrink.name);
+        }
     }
 }
