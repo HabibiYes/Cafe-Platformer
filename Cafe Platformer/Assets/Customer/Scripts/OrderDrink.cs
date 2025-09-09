5ad024d6610da90ffb33141420d04e35dbd08e5f
HabibiYes <calebfparr@gmail.com>
2025-09-09 13:57:31 -0500

    Icon logic added

--- Cafe Platformer/Assets/Customer/Scripts/OrderDrink.cs
+++ Cafe Platformer/Assets/Customer/Scripts/OrderDrink.cs
@@ -1,19 +1,29 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OrderDrink : MonoBehaviour
 {
     Customer customer;
 
+    private GameObject icon;
+
     [HideInInspector] public DrinkData orderedDrink;
 
     private void Awake()
     {
         customer = GetComponent<Customer>();
+
+        // Get and deactivate icon
+        icon = transform.Find("Icon").gameObject;
+        icon.SetActive(false);
     }
 
     public void Order()
     {
-        orderedDrink = GameData.Instance.drinks[Random.Range(0, GameData.Instance.drinks.Count)];
+        int index = Random.Range(0, GameData.Instance.drinks.Count);
+        orderedDrink = GameData.Instance.drinks[index];
+
+        // Set and activate icon
+        icon.GetComponent<MeshRenderer>().material.SetFloat("_Index", index);
+        icon.SetActive(true);
     }
 }
