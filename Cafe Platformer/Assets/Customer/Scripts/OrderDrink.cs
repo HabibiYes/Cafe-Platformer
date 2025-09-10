dcca2dfef732f7a6077dbbcb903849a9d75b526c
HabibiYes <calebfparr@gmail.com>
2025-09-09 21:23:54 -0500

    Customer order icons

--- Cafe Platformer/Assets/Customer/Scripts/OrderDrink.cs
+++ Cafe Platformer/Assets/Customer/Scripts/OrderDrink.cs
@@ -5,6 +5,7 @@ public class OrderDrink : MonoBehaviour
     Customer customer;
 
     private GameObject icon;
+    [SerializeField] private float iconCircleColorMultiplier = 0.5f;
 
     [HideInInspector] public DrinkData orderedDrink;
 
@@ -23,7 +24,9 @@ public class OrderDrink : MonoBehaviour
         orderedDrink = GameData.Instance.drinks[index];
 
         // Set and activate icon
-        icon.GetComponent<MeshRenderer>().material.SetFloat("_Index", index);
+        MeshRenderer meshRenderer = transform.Find("Icon").GetComponent<MeshRenderer>();
+        meshRenderer.material.SetFloat("_Index", index);
+        meshRenderer.material.SetColor("_Color", orderedDrink.color * iconCircleColorMultiplier);
         icon.SetActive(true);
     }
 }
